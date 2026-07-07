56cfe6158b23efd5108a4afa6fe498a0720d9442
r3kurs <86468530+r3kurs@users.noreply.github.com>
2026-07-07 12:10:30 +0000

    Add GPLv3 license header to Kotlin files; add license notice to help screen

--- app/src/main/java/de/investitionskalkulator/Kalkulator.kt
+++ app/src/main/java/de/investitionskalkulator/Kalkulator.kt
@@ -1,3 +1,17 @@
+/*
+ * Investment Calculator
+ * Copyright (C) 2026  r3kurs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the LICENSE file in the project directory for details.
+ */
+
 package de.investitionskalkulator
 
 import kotlin.math.floor
